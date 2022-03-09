7b61f9f3c2332bea44947175d06a8080ca12fec1
INFO5Cattedra <INFO5Cattedra@INFO5CATTEDRA>
2022-03-09 10:33:37 +0100

    inizioi gestion

--- frmMain.cs
+++ frmMain.cs
@@ -17,6 +17,29 @@ namespace AS2122_4E_INF_TogniEmanuele_Palestra
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int persone = Convert.ToInt32(txtPersone.Text);
+            int mesi = Convert.ToInt32(nudMesi.Text);
+            double preventivo = 0;
+            int prezzoSpinning = 40, prezzoDanza = 50, prezzoPesi = 45;
+
+            if(chkSpinning.Checked == true)
+            {
+                preventivo += prezzoSpinning * mesi*persone;
+            }
+            if (chkDanza.Checked == true)
+            {
+                preventivo += prezzoDanza * mesi;
+            }
+            if (chkPesi.Checked == true)
+            {
+                preventivo += prezzoPesi * mesi;
+            }
+
+            lblPreventivo.Text = preventivo.ToString();
+        }
+
         
     }
 }
